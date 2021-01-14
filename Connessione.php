056eb3e497ee1bb67db4912d203a739c31d55d51
Francesco <francescoaurisicchio9@gmail.com>
2021-01-14 15:08:16 +0100

    Connessione db

---
+++ Connessione.php
@@ -0,0 +1,9 @@
+$link = mysqli_connect("mydatabase", "root", "pass");
+    
+    
+    if($link === false){
+        die("ERROR: Could not connect. " . mysqli_connect_error());
+    }
+    
+    
+    echo "Connect Successfully. Host info: " . mysqli_get_host_info($link);
